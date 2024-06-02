548214c738dee7f87bded30939348cb89879134c
Nguyen Ngoc Bach <bachnn92@gmail.com>
2024-06-02 16:24:34 +0700

    update

---
+++ Deutsch.py
@@ -0,0 +1,27 @@
+print("Hallo")
+
+
+
+# def Gramatik
+
+# def Nomen = "der Hunt"
+
+
+# def Verben
+
+# def Adjectiv
+
+
+# def Nominativ
+
+# def Akkusativ
+
+# def Genitiv
+
+# def Sagen
+pronomen = ["ich","du", "er","sie","es", "Sie", "wir", "ihr", "sie", "Sie"]
+a=1 
+for p in pronomen:
+    print(p)
+
+869547046340576
